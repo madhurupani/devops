b3b5e1e96af424b159f1f55fbadbf789092074ec
madhurupani <36263373+madhurupani@users.noreply.github.com>
2018-07-15 10:51:55 +0530

    Update main.tf
    qvduqugd

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@
 #  region    = "us-east-1"
 #}
 
-terraform {
-  backend "s3" {
-    region  = "us-east-1"
-    bucket  = "practiceterra"
-    key     = "terraform.tfstate"
-    encrypt = true
-  }
-}
+#terraform {
+ # backend "s3" {
+  #  region  = "us-east-1"
+   # bucket  = "practiceterra"
+    #key     = "terraform.tfstate"
+    #encrypt = true
+  #}
+#}
