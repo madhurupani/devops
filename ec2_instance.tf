e92b5b6eece3b196ca49002f68759415307d81c7
root <root@ip-172-31-47-117.ec2.internal>
2018-07-15 04:23:08 +0000

    added

---
+++ ec2_instance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "madhu" {
+  ami           = "ami-43a15f3e"
+  instance_type = "t2.micro"
+#  region        = "us-east-1"
+  count         = 1
+}
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.madhu.id}"
+}
+
---
+++ main.tf
@@ -0,0 +1,12 @@
+#provider "aws" {
+#  region    = "us-east-1"
+#}
+
+terraform {
+  backend "s3" {
+    region  = "us-east-1"
+    bucket  = "practiceterra"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+}
